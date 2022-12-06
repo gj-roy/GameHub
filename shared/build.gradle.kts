3e4872d9f6bedb8c928632696330281e18388f6f
hojat <hojat72elect@gmail.com>
2022-12-06 17:54:50 -0500

    Ticket_2 : Added gradle script of [core] module to [shared] module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -7,10 +7,37 @@ plugins {
 
 android {
     namespace = "ca.on.hojat.gamenews.shared"
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+    }
 }
 
 dependencies {
 
+    implementation(project(deps.local.commonDomain))
+    testImplementation(project(deps.local.commonTesting))
+
+
+
+    implementation(deps.kotlin.coroutines)
+    implementation(deps.kotlin.serialization)
+    implementation(deps.androidX.browser)
     implementation(deps.commons.core)
     implementation(deps.commons.ktx)
+    implementation(deps.commons.network)
+    implementation(deps.commons.windowAnims)
+    implementation(deps.misc.kotlinResult)
+    implementation(deps.google.daggerHiltAndroid)
+    kapt(deps.google.daggerHiltAndroidCompiler)
+    implementation(deps.misc.hiltBinder)
+    ksp(deps.misc.hiltBinderCompiler)
+    coreLibraryDesugaring(deps.misc.desugaredLibs)
+    testImplementation(deps.testing.jUnit)
+    testImplementation(deps.testing.truth)
+    testImplementation(deps.testing.mockk)
+    testImplementation(deps.testing.coroutines)
+    androidTestImplementation(deps.testing.testRunner)
+    androidTestImplementation(deps.testing.jUnitExt)
+
 }
