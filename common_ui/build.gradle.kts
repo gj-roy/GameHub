262502c7dbf03d5fdb2b2ae97a4421b34a55bbcb
hojat <hojat72elect@gmail.com>
2022-12-16 17:37:46 -0500

    Ticket_6 : Created [common_ui] module

---
+++ common_ui/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id(PLUGIN_ANDROID_LIBRARY)
+    id(PLUGIN_GAMENEWS_ANDROID)
+}
+
+android {
+    namespace = "ca.on.hojat.gamenews.common_ui"
+
+
+    defaultConfig {
+
+    }
+
+}
+
+dependencies {
+
+
+}
