c6b3cdd2059d91f51f661fd2386fff9b902c1811
hojat <hojat72elect@gmail.com>
2022-12-16 01:01:25 -0500

    Ticket_6 : Cleaned unnecessary stuff

--- shared/src/main/java/ca/on/hojat/gamenews/shared/extensions/Drawable.kt
+++ shared/src/main/java/ca/on/hojat/gamenews/shared/extensions/Drawable.kt
@@ -2,16 +2,12 @@
 
 package ca.on.hojat.gamenews.shared.extensions
 
-import android.content.res.ColorStateList
-import android.graphics.Bitmap
-import android.graphics.Canvas
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffColorFilter
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.GradientDrawable
 import android.graphics.drawable.RippleDrawable
 import androidx.annotation.ColorInt
-import androidx.core.graphics.drawable.DrawableCompat
 
 fun Drawable.setColor(@ColorInt color: Int): Drawable {
     return when (val newDrawable = mutate()) {
@@ -24,56 +20,3 @@ fun Drawable.setColor(@ColorInt color: Int): Drawable {
     }
 }
 
-fun Drawable.setGradientColors(
-    @ColorInt startColor: Int,
-    @ColorInt endColor: Int
-): Drawable {
-    return if (this is GradientDrawable) {
-        mutate().apply { colors = intArrayOf(startColor, endColor) }
-    } else {
-        this
-    }
-}
-
-fun Drawable.setSize(size: Int) {
-    applyBounds(
-        left = 0,
-        top = 0,
-        right = size,
-        bottom = size
-    )
-}
-
-fun Drawable.applyBounds(
-    left: Int,
-    top: Int,
-    right: Int,
-    bottom: Int
-): Drawable {
-    return apply { setBounds(left, top, right, bottom) }
-}
-
-fun Drawable.setTintList(states: Array<IntArray>, colors: IntArray) {
-    DrawableCompat.setTintList(
-        DrawableCompat.wrap(this),
-        ColorStateList(states, colors)
-    )
-}
-
-fun Drawable.toBitmap(
-    config: Bitmap.Config = Bitmap.Config.ARGB_8888
-): Bitmap {
-    return Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, config)
-        .also { bitmap ->
-            val canvas = Canvas(bitmap)
-
-            applyBounds(
-                left = 0,
-                top = 0,
-                right = canvas.width,
-                bottom = canvas.height
-            )
-
-            draw(canvas)
-        }
-}
--- shared/src/main/java/ca/on/hojat/gamenews/shared/extensions/Float.kt
+++ shared/src/main/java/ca/on/hojat/gamenews/shared/extensions/Float.kt
@@ -2,23 +2,6 @@ package ca.on.hojat.gamenews.shared.extensions
 
 import android.content.Context
 
-fun Float.dpToPx(context: Context): Float {
-    return (this * context.displayMetrics.density)
-}
-
-
-fun Float.spToPx(context: Context): Float {
-    return (this * context.displayMetrics.scaledDensity)
-}
-
-
 fun Float.pxToDp(context: Context): Float {
     return (this / context.displayMetrics.density)
 }
-
-
-fun Float.pxToSp(context: Context): Float {
-    return (this / context.displayMetrics.scaledDensity)
-}
-
-
