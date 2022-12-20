35d9e5fbf654be0c6274e5e6232e5a047648f0a5
hojat <hojat72elect@gmail.com>
2022-12-20 15:45:38 -0500

    Ticket_6 : Cleaned unnecessary stuff

--- core/src/main/java/ca/on/hojat/gamenews/core/SdkInfo.kt
+++ core/src/main/java/ca/on/hojat/gamenews/core/SdkInfo.kt
@@ -5,12 +5,6 @@ import androidx.annotation.ChecksSdkIntAtLeast
 
 object SdkInfo {
 
-    @JvmField
-    val SDK_VERSION = Build.VERSION.SDK_INT
-
-    @JvmField
-    val IS_AT_LEAST_OREO = (SDK_VERSION >= Build.VERSION_CODES.O)
-
     @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.R)
     @JvmField
     val IS_AT_LEAST_11 = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
--- core/src/main/java/ca/on/hojat/gamenews/core/extensions/TypedArray.kt
+++ core/src/main/java/ca/on/hojat/gamenews/core/extensions/TypedArray.kt
@@ -2,31 +2,10 @@
 
 package ca.on.hojat.gamenews.core.extensions
 
-import android.annotation.SuppressLint
-import android.content.Context
 import android.content.res.TypedArray
-import android.graphics.Typeface
 import androidx.annotation.StyleableRes
-import androidx.core.content.res.ResourcesCompat
-import ca.on.hojat.gamenews.core.SdkInfo
-
 
 fun TypedArray.getString(@StyleableRes index: Int, default: CharSequence = ""): CharSequence {
     return (getString(index) ?: default)
 }
 
-@SuppressLint("NewApi")
-fun TypedArray.getFont(
-    context: Context,
-    @StyleableRes index: Int,
-    default: Typeface
-): Typeface {
-    return if (SdkInfo.IS_AT_LEAST_OREO) {
-        (getFont(index) ?: default)
-    } else {
-        getResourceId(index, -1)
-            .takeIf { it != -1 }
-            ?.let { ResourcesCompat.getFont(context, it) }
-            ?: default
-    }
-}
