b4319d3b8f909ef85518c940f9977745b30d67f5
hojat <hojat72elect@gmail.com>
2022-12-22 00:45:00 -0500

    Ticket_6 : some cleaning

--- shared/src/main/java/ca/on/hojat/gamenews/shared/api/ApiErrorMapper.kt
+++ shared/src/main/java/ca/on/hojat/gamenews/shared/api/ApiErrorMapper.kt
@@ -1,17 +1,9 @@
 package ca.on.hojat.gamenews.shared.api
 
 import ca.on.hojat.gamenews.core.domain.entities.Error
-import ca.on.hojat.gamenews.shared.api.common.httpErrorMessage
-import ca.on.hojat.gamenews.shared.api.common.isHttpError
-import ca.on.hojat.gamenews.shared.api.common.isNetworkError
-import ca.on.hojat.gamenews.shared.api.common.isServerError
-import ca.on.hojat.gamenews.shared.api.common.isUnknownError
-import ca.on.hojat.gamenews.shared.api.common.networkErrorMessage
-import ca.on.hojat.gamenews.shared.api.common.unknownErrorMessage
 import javax.inject.Inject
 import kotlin.IllegalStateException
 import kotlin.with
-
 import ca.on.hojat.gamenews.shared.api.common.Error as ApiError
 
 class ApiErrorMapper @Inject constructor() {
--- shared/src/main/java/ca/on/hojat/gamenews/shared/api/common/Error.kt
+++ shared/src/main/java/ca/on/hojat/gamenews/shared/api/common/Error.kt
@@ -6,25 +6,25 @@ sealed class Error {
     data class HttpError(val code: Int, val message: String) : Error()
     data class NetworkError(val throwable: Throwable) : Error()
     data class UnknownError(val throwable: Throwable) : Error()
+
+    val isHttpError: Boolean
+        get() = (this is HttpError)
+
+    val isServerError: Boolean
+        get() = ((this is HttpError) && (code >= HttpCodes.SERVER_ERROR))
+
+    val isNetworkError: Boolean
+        get() = (this is NetworkError)
+
+    val isUnknownError: Boolean
+        get() = (this is UnknownError)
+
+    val httpErrorMessage: String
+        get() = (if (this is HttpError) message else "")
+
+    val networkErrorMessage: String
+        get() = (if (this is NetworkError) (throwable.message ?: "") else "")
+
+    val unknownErrorMessage: String
+        get() = (if (this is UnknownError) (throwable.message ?: "") else "")
 }
-
-val Error.isHttpError: Boolean
-    get() = (this is Error.HttpError)
-
-val Error.isServerError: Boolean
-    get() = ((this is Error.HttpError) && (code >= HttpCodes.SERVER_ERROR))
-
-val Error.isNetworkError: Boolean
-    get() = (this is Error.NetworkError)
-
-val Error.isUnknownError: Boolean
-    get() = (this is Error.UnknownError)
-
-val Error.httpErrorMessage: String
-    get() = (if (this is Error.HttpError) message else "")
-
-val Error.networkErrorMessage: String
-    get() = (if (this is Error.NetworkError) (throwable.message ?: "") else "")
-
-val Error.unknownErrorMessage: String
-    get() = (if (this is Error.UnknownError) (throwable.message ?: "") else "")
