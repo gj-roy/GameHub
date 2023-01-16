24ea7b125f4230f5d358b9ed0310ea55b74d1a23
hojat <hojat72elect@gmail.com>
2023-01-15 20:42:43 -0500

    Ticket_3 : Fixed the bug of the news section

--- app/src/main/java/ca/on/hojat/gamenews/core/data/database/articles/DbImageType.kt
+++ app/src/main/java/ca/on/hojat/gamenews/core/data/database/articles/DbImageType.kt
@@ -1,5 +1,12 @@
 package ca.on.hojat.gamenews.core.data.database.articles
 
+/**
+ * I know these enums aren't used anywhere, but don't remove them.
+ */
 enum class DbImageType {
     UNKNOWN,
+    SQUARE_TINY,
+    SQUARE_SMALL,
+    SCREEN_TINY,
+    ORIGINAL
 }
