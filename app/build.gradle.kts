5d1e0fe7f3cf85513b40cdc683ec5e21063d3f03
hojat <hojat72elect@gmail.com>
2023-01-22 20:56:17 -0500

    Ticket_7 : Added YouTube API key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,8 @@ android {
 
 dependencies {
 
+    implementation(files("libs/YouTubeAndroidPlayerApi.jar"))
+
     // general jetpack libs
     implementation(AndroidX.protoDataStore)
     implementation(AndroidX.splash)
