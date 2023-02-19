754e6e940b1edfdf5b0e7b3ca3468513d9bf22ed
hojat <hojat72elect@gmail.com>
2023-02-18 19:04:23 -0500

    Reformatting

--- app/src/main/java/ca/on/hojat/gamenews/feature_settings/domain/entities/Theme.kt
+++ app/src/main/java/ca/on/hojat/gamenews/feature_settings/domain/entities/Theme.kt
@@ -1,7 +1,16 @@
 package ca.on.hojat.gamenews.feature_settings.domain.entities
 
+import ca.on.hojat.gamenews.R
+
 enum class Theme {
     LIGHT,
     DARK,
     SYSTEM,
 }
+
+internal val Theme.uiTextRes: Int
+    get() = when (this) {
+        Theme.LIGHT -> R.string.settings_item_theme_option_light
+        Theme.DARK -> R.string.settings_item_theme_option_dark
+        Theme.SYSTEM -> R.string.settings_item_theme_option_system_default
+    }
