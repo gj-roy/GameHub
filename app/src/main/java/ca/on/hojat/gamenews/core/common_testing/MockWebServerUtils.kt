df4cf1ffe49f54e20f5b68d818d23d1270fe1951
hojat <hojat72elect@gmail.com>
2023-03-23 01:15:14 -0400

    Cleaned

--- app/src/main/java/ca/on/hojat/gamenews/core/common_testing/MockWebServerUtils.kt
+++
@@ -1,9 +0,0 @@
-package ca.on.hojat.gamenews.core.common_testing
-
-import okhttp3.mockwebserver.MockWebServer
-
-fun MockWebServer.startSafe() = try {
-    start()
-} catch (ignore: Throwable) {
-    // ignore
-}
