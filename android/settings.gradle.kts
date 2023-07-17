43d0f194a2781baaffe4df121744329ed921690a
hojat <hojat72elect@gmail.com>
2023-07-17 15:38:14 -0400

    Added required dependencies to the settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,3 +1,5 @@
+import groovy.lang.Closure
+
 rootProject.name = "GameNews"
 
 pluginManagement {
@@ -16,4 +18,14 @@ dependencyResolutionManagement {
 }
 
 include(":app")
+
+// including the RN's native android code:
 includeBuild("../node_modules/@react-native/gradle-plugin")
+// including our own native android code:
+apply(from = file("../node_modules/@react-native-community/cli-platform-android/native_modules.gradle"))
+val applyNativeModules: Closure<Any> = extra.get("applyNativeModulesSettingsGradle") as Closure<Any>
+applyNativeModules(settings)
+
+
+
+
