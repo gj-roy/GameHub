467f1762fd2679eee054cf00f362c6bd99d75a77
hojat <hojat72elect@gmail.com>
2023-07-17 01:07:35 -0400

    Updated the version of AGP and Gradle.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.2")
+        classpath("com.android.tools.build:gradle:7.4.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
 
         // This should always be version of "coreHiltVersion".
