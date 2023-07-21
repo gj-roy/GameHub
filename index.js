b4b1ceb6c35e33e596ecb9aa031f8870af4e7ee6
hojat <hojat72elect@gmail.com>
2023-07-21 19:29:01 -0400

    Converted index.tsx back to js.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import {AppRegistry, StyleSheet, Text, View} from 'react-native';
 const HelloWorld = () => {
     return (
         <View style={styles.container}>
-            <Text style={styles.hello}>Hello, World</Text>
+            <Text style={styles.hello}>{"Hello world\nMy name is Hojat\nAnd I live in Canada"}</Text>
         </View>
     );
 };
@@ -12,11 +12,13 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
+        alignItems: 'center'
     },
     hello: {
         fontSize: 20,
-        textAlign: 'center',
+        textAlign: 'left',
         margin: 10,
+        width: 'auto'
     },
 });
 
