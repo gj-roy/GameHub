8558cfbb34b907aabef782e0379a99e3cc52d214
hojat <hojat72elect@gmail.com>
2023-07-24 17:25:34 -0400

    Created a row of games preview cards.

--- src/DiscoverScreen.tsx
+++ src/DiscoverScreen.tsx
@@ -1,14 +1,33 @@
-import {View, Text} from 'react-native'
+import {View} from 'react-native'
 import React from 'react'
+import {GamesCategoryPreview, GamesCategoryPreviewDataModel} from "./ui/GamesCategoryPreview";
 
 export const DiscoverScreen = () => {
+
+    const fakeGames: GamesCategoryPreviewDataModel[] = [
+        {id: 1, title: "Super Catboy", coverUrl: "https://images.igdb.com/igdb/image/upload/t_cover_big/co42mw.jpg"},
+        {
+            id: 2,
+            title: "The Legend of Zelda: Tears of the Kingdom",
+            coverUrl: "https://images.igdb.com/igdb/image/upload/t_cover_big/co5vmg.jpg"
+        },
+        {
+            id: 3,
+            title: "Street Fighter 6",
+            coverUrl: "https://images.igdb.com/igdb/image/upload/t_cover_big/co5vst.jpg"
+        },
+        {
+            id: 4,
+            title: "The Banished Vault",
+            coverUrl: "https://images.igdb.com/igdb/image/upload/t_cover_big/co69qx.jpg"
+        },
+    ];
+
     return (
         <View style={{
             flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center'
         }}>
-            <Text>DiscoverScreen</Text>
+            <GamesCategoryPreview title="popular" games={fakeGames}/>
         </View>
     )
 }
---
+++ src/ui/GamePreviewCard.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Image} from "react-native";
+import {GamesCategoryPreviewDataModel} from "./GamesCategoryPreview";
+import {ImageURISource} from "react-native/Libraries/Image/ImageSource";
+
+
+export type GamePreviewCardProps = {
+    game: GamesCategoryPreviewDataModel;
+
+};
+
+/**
+ * A card in the categorized list of games, which gives some info about the game.
+ * And if clicked, leads the user to game's specific page.
+ */
+export const GamePreviewCard = ({game}: GamePreviewCardProps) => {
+
+    const gameCoverUrl: ImageURISource = {uri: game.coverUrl ?? undefined}
+    return (
+        <Image
+            style={{
+                width: 130,
+                height: 160,
+                borderRadius: 8,
+                marginRight: 5,
+                resizeMode: 'cover'
+            }}
+            source={gameCoverUrl}
+        />
+    );
+}
