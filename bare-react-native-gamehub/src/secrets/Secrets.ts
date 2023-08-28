64910753d61cfd4cd89db9c4ab9943370ae2d5b3
hojat <hojat72elect@gmail.com>
2023-08-27 23:23:11 -0400

    refactored the Android app to "legacy-native-android-gamehub" folder and the first migration codes to "bare-react-native-gamehub" directory.

---
+++ bare-react-native-gamehub/src/secrets/Secrets.ts
@@ -0,0 +1,8 @@
+export const GAMESPOT_API_KEY: string = "19a46433a66816e16e6a5069adef5dec9fc0f5fd";
+export const GAMESPOT_API_BASE_URL: string = "https://www.gamespot.com/api";
+export const TWITCH_API_BASE_URL: string = "https://id.twitch.tv/";
+export const IGDB_API_BASE_URL: string = "https://api.igdb.com/v4";
+
+export const TWITCH_APP_CLIENT_ID: string = "g88ty1lcsaf2ewvqqpyxngpssbvhsq";
+
+export const TWITCH_APP_CLIENT_SECRET: string = "erupt1f7d6b4kkyaakwhp50v8wf09v";
