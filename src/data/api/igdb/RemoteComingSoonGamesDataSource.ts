6d3663c4aa934e485cf1d22402ea5bdb338fc37a
hojat <hojat72elect@gmail.com>
2023-09-02 00:02:28 -0400

    Some improvements to the DataSources in the API layer for IGDB service.

--- src/data/api/igdb/RemoteComingSoonGamesDataSource.ts
+++ src/data/api/igdb/RemoteComingSoonGamesDataSource.ts
@@ -1,12 +1,13 @@
 import {IgdbService} from "./IgdbService";
 import {getAllRequestFields} from "./RequestFieldsRepository";
+import {ApiGame} from "./entities/ApiGame";
 
 export class RemoteComingSoonGamesDataSource {
 
     constructor() {
     }
 
-    getComingSoonGames() {
+    getComingSoonGames(): Promise<ApiGame[]> {
 
         return IgdbService(`${getAllRequestFields()};where first_release_date > 1691193575 ;limit 20;sort first_release_date asc;`);
     }
--- src/data/api/igdb/RemotePopularGamesDataSource.ts
+++ src/data/api/igdb/RemotePopularGamesDataSource.ts
@@ -1,12 +1,13 @@
 import {IgdbService} from "./IgdbService";
 import {getAllRequestFields} from "./RequestFieldsRepository";
+import {ApiGame} from "./entities/ApiGame";
 
 export class RemotePopularGamesDataSource {
 
     constructor() {
     }
 
-    getPopularGames() {
+    getPopularGames(): Promise<ApiGame[]> {
 
         return IgdbService(`${getAllRequestFields()};where rating != null;where first_release_date > 1538129354;limit 20;sort total_rating desc;`);
     }
